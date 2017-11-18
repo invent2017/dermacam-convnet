5d2c4ee5fa973b673e43b359f33a90a63e85329e
Filbert <fillberth@gmail.com>
2017-11-18 23:46:12 +0800

    removed .ipynb_checkpoints, updated exported .py file

--- py/conv_tf.py
+++ py/conv_tf.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[25]:
+# In[1]:
 
 import tensorflow as tf
 import pandas as pd
@@ -15,7 +15,7 @@ print(tf.__version__)
 tf.logging.set_verbosity("INFO")
 
 
-# In[46]:
+# In[7]:
 
 # define preprocessing function
 def preprocess(disease, first, last, data, label):
@@ -26,16 +26,18 @@ def preprocess(disease, first, last, data, label):
         for x in range(0,64):
             for y in range(0,64):
                 rgb.append(pic[x,y])
-        
-        #rgb.append(red)
-        #rgb.append(green)
-        #rgb.append(blue)
 
         data.append(rgb)
         if disease == "acne":
             label.append(0)
         elif disease == "ezcema":
             label.append(1)
+        elif disease == "psoriasis":
+            label.append(2)
+        elif disease == "seborrheic keratoses":
+            label.append(3)
+        elif disease == "skin cancer":
+            label.append(4)
 
 # define training data
 train_data = []
@@ -43,6 +45,9 @@ train_labels = []
 
 preprocess("acne",1,48,train_data,train_labels)
 preprocess("ezcema",1,51,train_data,train_labels)
+preprocess("psoriasis",1,44,train_data,train_labels)
+preprocess("seborrheic keratoses",1,45,train_data,train_labels)
+preprocess("skin cancer",1,45,train_data,train_labels)
     
 train_data = np.asarray(train_data, dtype=np.float32)
 train_labels = np.asarray(train_labels)
@@ -53,15 +58,24 @@ test_labels = []
 
 preprocess("acne",49,53,test_data,test_labels)
 preprocess("ezcema",52,56,test_data,test_labels)
+preprocess("psoriasis",45,49,test_data,test_labels)
+preprocess("seborrheic keratoses",46,50,test_data,test_labels)
+preprocess("skin cancer",46,50,test_data,test_labels)
 
 test_data = np.asarray(test_data, dtype=np.float32)
 test_labels = np.asarray(test_labels)
 
 
-# In[1]:
+# In[8]:
+
+print(train_data.shape, train_labels.shape)
+print(test_data.shape, test_labels.shape)
+
+
+# In[16]:
 
 # define network architecture
-def cnn_model_fn(features, labels, mode):
+def cnn_model_fn1(features, labels, mode):
     """Model function for CNN."""
     # Input Layer
     input_layer = tf.reshape(features["x"], [-1, 64, 64, 3])
@@ -90,7 +104,7 @@ def cnn_model_fn(features, labels, mode):
     dropout = tf.layers.dropout(inputs=dense, rate=0.1, training=mode == tf.estimator.ModeKeys.TRAIN)
     
     # Logits Layer
-    logits = tf.layers.dense(inputs=dropout, units=2)
+    logits = tf.layers.dense(inputs=dropout, units=5)
     
     predictions = {
         "classes": tf.argmax(input=logits, axis=1),
@@ -101,7 +115,7 @@ def cnn_model_fn(features, labels, mode):
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
     
     # Loss Calculation
-    onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=2)
+    onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=5)
     loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits)
     
     # Configure training op
@@ -116,31 +130,100 @@ def cnn_model_fn(features, labels, mode):
     }
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
-# create dermacam model
-dermacam = tf.estimator.Estimator(model_fn=cnn_model_fn)
+
+# In[32]:
+
+# define network architecture
+def cnn_model_fn2(features, labels, mode):
+    """Model function for CNN."""
+    # Input Layer
+    input_layer = tf.reshape(features["x"], [-1, 64, 64, 3])
+    
+    # Convolutional Layer #1
+    conv1 = tf.layers.conv2d(
+      inputs=input_layer,
+      filters=64,
+      kernel_size=[16, 16],
+      padding="same",
+      activation=tf.nn.relu) #output: [-1, 64, 64, 64]
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2) #output [-1, 32, 32, 64]
+    #print(conv1.shape, pool1.shape)
+    
+    # Convolutional Layer #2
+    conv2 = tf.layers.conv2d(
+      inputs=pool1,
+      filters=128,
+      kernel_size=[12, 12],
+      padding="same",
+      activation=tf.nn.relu) #output: [-1, 32, 32, 128]
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2) #output [-1, 16, 16, 128]
+    #print(conv2.shape, pool2.shape)
+    
+    # Dense Layers
+    pool2_flat = tf.reshape(pool2, [-1, 16 * 16 * 128])
+    dense1 = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+    dropout1 = tf.layers.dropout(inputs=dense1, rate=0.1, training=mode == tf.estimator.ModeKeys.TRAIN)
+    dense2 = tf.layers.dense(inputs=dropout1, units=512, activation=tf.nn.relu)
+    dropout2 = tf.layers.dropout(inputs=dense2, rate=0.1, training=mode == tf.estimator.ModeKeys.TRAIN)
+    
+    # Logits Layer
+    logits = tf.layers.dense(inputs=dropout2, units=5)
+    
+    predictions = {
+        "classes": tf.argmax(input=logits, axis=1),
+        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+    }
+    
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+    
+    # Loss Calculation
+    onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=5)
+    loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits)
+    
+    # Configure training op
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+    
+    # Add Evaluation Metrics
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])
+    }
+    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+
+# In[33]:
+
+# Create dermacam model
+# there are 2 models, one is the original one, the other is the new one im experimenting with
+
+#dermacam = tf.estimator.Estimator(model_fn=cnn_model_fn1,model_dir="models/dermacam1")
+dermacam = tf.estimator.Estimator(model_fn=cnn_model_fn2,model_dir="models/dermacam2")
 
 # Set up logging for predictions
 tensors_to_log = {"probabilities": "softmax_tensor"}
 logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=100)
 
 
-# In[42]:
+# In[ ]:
 
 # Train the model
 train_input_fn = tf.estimator.inputs.numpy_input_fn(
     x={"x": train_data},
     y=train_labels,
-    batch_size=10,
+    batch_size=20,
     num_epochs=None,
     shuffle=True)
 
 dermacam.train(
     input_fn=train_input_fn,
-    steps=500,
+    steps=2,
     hooks=[logging_hook])
 
 
-# In[43]:
+# In[29]:
 
 # Evaluate the model and print results
 eval_input_fn = tf.estimator.inputs.numpy_input_fn(
